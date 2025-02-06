058c33aedfb9553beb44b9c8bda009fbfc9fa068
ibrahim kedir <techofreact@gmail.com>
2025-02-06 20:08:05 +0300

    ft: done with hero section.

--- src/_components/Hero/LeftSection.hero.tsx
+++ src/_components/Hero/LeftSection.hero.tsx
@@ -1,17 +1,17 @@
 export default function HeroLeft() {
-    return (
-        <aside className="hero_title_button_container flex flex-col  gap-y-5">
-            <p className="font-medium text-5xl leading-[120%] text-white">
-                Unlock Your Business Potential with Our Expert Solutions.
-            </p>
-            <section className="flex gap-3">
-                <div className="bg-white text-gray-900 text-sm font-semibold px-4 py-1 rounded-lg">
-                    100K+ Our Clients Here
-                </div>
-                <div className="bg-white text-gray-900 text-sm font-semibold px-4 py-1 rounded-lg">
-                    100K+ Our Clients Here
-                </div>
-            </section>
-        </aside>
-    );
+  return (
+    <aside className="hero_title_button_container flex flex-col  gap-y-5">
+      <p className="font-medium text-5xl leading-[120%] text-white">
+        Unlock Your Business Potential with Our Expert Solutions.
+      </p>
+      <section className="flex gap-3">
+        <div className="bg-white text-gray-900 text-sm font-semibold px-4 py-1 rounded-lg">
+          100K+ Our Clients Here
+        </div>
+        <div className="bg-white text-gray-900 text-sm font-semibold px-4 py-1 rounded-lg">
+          100K+ Our Clients Here
+        </div>
+      </section>
+    </aside>
+  );
 }
