e1f4cadbdb1c1c53aca0a081811c8faf6c6d26d4
ibrahim kedir <techofreact@gmail.com>
2025-02-10 14:18:47 +0300

    ui: fix top nav background color on scroll.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -19,31 +19,34 @@ export default function Home() {
       </section>
       {/* card section */}
       <section className="px-3 md:px-14 main_card_wrapper flex flex-col gap-4 -mt-20">
-        <p className="font-bold text-xl">
-          አዲስ ነገር ኤግዚቢሽን መግቢያ ትኬቶች
-        </p>
+        <p className="font-bold text-xl">አዲስ ነገር ኤግዚቢሽን መግቢያ ትኬቶች</p>
         <article className="card_wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {
-            Array(3).fill(0).map((_, key) => (
+          {Array(3)
+            .fill(0)
+            .map((_, key) => (
               <article className="max-w-sm rounded overflow-hidden shadow-lg" key={key}>
-                <Image width={200} height={200} className="w-full" src="/hero_exhibition_placeholder.jpeg" alt="Sunset in the mountains" />
+                <Image
+                  width={200}
+                  height={200}
+                  className="w-full"
+                  src="/hero_exhibition_placeholder.jpeg"
+                  alt="Sunset in the mountains"
+                />
                 <div className="px-6 py-4">
                   <div className="font-bold text-xl mb-2">
-                    <p>
-                      አዲስ ነገር ኤክስፖ
-                    </p>
+                    <p>አዲስ ነገር ኤክስፖ</p>
                   </div>
                   <p className="text-gray-700 text-base">
-                    የአዲስ ነገር ኤክስፖን የመግቢያ ትኬት ቀድመዉ 100 ብር ብቻ ! በመግዛት የልዩ ቅናሽ ተጠቃሚ ይሁኑ፡፡ አዲስ ነገር ኤክስፖ በዉስጡ የተለያዩ የሽመታ፣ የመዝናኛ፣ የምግብ እና መጠጥ፣ የጤና እና ዉበት፣ የቢዝነስ እና ሪልእስቴት መንደሮችን የያዘ ሲሆን የአዲስ መኪና ሽልማት እንዲሁም በየቀኑ አዳዲስ ልዩ ሽልማቶች አሉት ፡፡ እርስዎ እድለኛ ቢሆኑስ ?                  </p>
+                    የአዲስ ነገር ኤክስፖን የመግቢያ ትኬት ቀድመዉ 100 ብር ብቻ ! በመግዛት የልዩ ቅናሽ ተጠቃሚ ይሁኑ፡፡ አዲስ ነገር ኤክስፖ
+                    በዉስጡ የተለያዩ የሽመታ፣ የመዝናኛ፣ የምግብ እና መጠጥ፣ የጤና እና ዉበት፣ የቢዝነስ እና ሪልእስቴት መንደሮችን የያዘ ሲሆን
+                    የአዲስ መኪና ሽልማት እንዲሁም በየቀኑ አዳዲስ ልዩ ሽልማቶች አሉት ፡፡ እርስዎ እድለኛ ቢሆኑስ ?{' '}
+                  </p>
                 </div>
                 <div className="px-6 pt-4 pb-2">
-                  <Button className="text-white px-4 py-5 bg-[#f59c38]">
-                    ትኬትዎን ይቁረጡ
-                  </Button>
+                  <Button className="text-white px-4 py-5 bg-[#f59c38]">ትኬትዎን ይቁረጡ</Button>
                 </div>
               </article>
-            ))
-          }
+            ))}
         </article>
       </section>
     </main>
