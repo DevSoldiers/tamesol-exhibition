c5f86bfb2ce33df9c3665baa0c710bc76ac3578b
ibrahim kedir <techofreact@gmail.com>
2025-02-12 11:38:41 +0300

    ui-ft: supplier logos.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -68,6 +68,34 @@ export default function Home() {
         <p className="mb-3 text-white p-0 text-3xl text-center md:text-left">እየተዝናኑ ይሸምቱ!</p>
         <YznanuCards />
       </section>
+
+      {/* aqrabi drjtoch */}
+      <section className="aqrabi_drjtoch_section px-3 md:px-14 flex flex-col gap-4 bg-[rgb(247,168,56)] md:text-[64px] py-4 md:py-[80px]">
+        <p className="mb-3 text-white p-0 text-3xl text-center md:text-left">አቅራቢ ድርጅቶች</p>
+        <article className="flex flex-wrap justify-center gap-6 p-6 bg-white rounded-md">
+          <Image
+            src="/supplier_logos/lyan.svg"
+            width={200}
+            height={200}
+            alt="company logos"
+            className="w-32 sm:w-40 md:w-48 lg:w-52"
+          />
+          <Image
+            src="/supplier_logos/Tamcon_Solutions_Logo.png"
+            width={200}
+            height={200}
+            alt="company logos"
+            className="w-32 sm:w-40 md:w-48 lg:w-52"
+          />
+          <Image
+            src="/supplier_logos/Tamesol_Communications_Logo.png"
+            width={200}
+            height={200}
+            alt="company logos"
+            className="w-32 sm:w-40 md:w-48 lg:w-52"
+          />
+        </article>
+      </section>
     </main>
   );
 }
