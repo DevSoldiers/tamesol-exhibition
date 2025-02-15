4fe8472255fff0c4976dee3f57136f448bd491c3
ibrahim kedir <techofreact@gmail.com>
2025-02-15 12:36:12 +0300

    fix: missing image.

--- src/components/ui/YznanuYshemtu.tsx
+++ src/components/ui/YznanuYshemtu.tsx
@@ -2,6 +2,8 @@ import { useTranslations } from 'next-intl';
 
 export default function YznanuYshemtu() {
   const t = useTranslations();
+  const url = 'https://res.cloudinary.com/dzsa1ehta/image/upload';
+
   return (
     <section className="yznanu_yshemtu_section px-3 md:px-14 mt-5 flex flex-col gap-4 bg-white md:text-[64px] py-7 md:py-[80px]">
       <p className="mb-3 text-black p-0 text-3xl text-center md:text-left">
@@ -17,7 +19,7 @@ export default function YznanuYshemtu() {
             Your browser does not support the video tag.
           </video>
         </article>
-        <article className="w-full h-[400px] rounded-md bg-exhibition-img bg-cover"></article>
+        <article className="w-full h-[400px] rounded-md bg-exhibition-img bg-cover" style={{ backgroundImage: `url(${url}/advert_1)` }}></article>
       </div>
     </section>
   );
