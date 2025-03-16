a10e1762ee046b13a3dbbe6c159ff3b90ef2f4e3
ibrahim kedir <techofreact@gmail.com>
2025-03-16 19:46:23 +0300

    refactor: webTicket function to have interface.

--- src/app/[locale]/web-ticket/page.tsx
+++ src/app/[locale]/web-ticket/page.tsx
@@ -1,11 +1,19 @@
 import WebTicketPage from '@/_components/TicketPage/webTicket';
+import { NextPage } from 'next';
 
-export default async function WebTicket({ searchParams }: { searchParams: { ticket: string } }) {
-  const param = await searchParams;
-  const { ticket } = param ?? {};
+interface WebTicketProps {
+  searchParams: {
+    ticket?: string;
+  };
+}
+
+const WebTicket: NextPage<WebTicketProps> = ({ searchParams }) => {
+  const { ticket } = searchParams ?? {};
   return (
     <div className="bg-orange-300 -mt-[95px]">
-      <WebTicketPage isTsomFest={ticket == 'የፆም ፌስት'} />
+      <WebTicketPage isTsomFest={ticket === 'የፆም ፌስት'} />
     </div>
   );
-}
+};
+
+export default WebTicket;
