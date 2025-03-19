f1bd45af72983566a64f476623c0ab7ee4f64837
ibrahim kedir <techofreact@gmail.com>
2025-03-19 17:21:28 +0300

    refactor: suppliers logo alignment and size.

--- src/_components/ui/Supplier_Companies.tsx
+++ src/_components/ui/Supplier_Companies.tsx
@@ -5,27 +5,21 @@ export default function Suppliers() {
   return (
     <Container className=" bg-white md:text-[64px] py-4">
       <p className="mb-3 text-black p-0 text-3xl text-center md:text-left">አቅራቢ ድርጅቶች</p>
-      <article className="flex flex-wrap justify-center gap-6 p-6 bg-white rounded-md">
+      <article className="flex flex-wrap max-sm:flex-col justify-center gap-6 p-6 bg-white rounded-md items-center">
         <Image
           src="/supplier_logos/lyan.svg"
-          width={200}
-          height={200}
+          width={0}
+          height={0}
           alt="company logos"
-          className="w-32 sm:w-40 md:w-48 lg:w-52"
+          className="w-72"
         />
+        <Image src="/Tamcon_SVG.svg" width={0} height={0} alt="company logos" className="w-72" />
         <Image
-          src="/supplier_logos/Tamcon_Solutions_Logo.png"
-          width={200}
-          height={200}
+          src="/tamcon_tamesol.svg"
+          width={0}
+          height={0}
           alt="company logos"
-          className="w-32 sm:w-40 md:w-48 lg:w-52"
-        />
-        <Image
-          src="/supplier_logos/Tamesol_Communications_Logo.png"
-          width={200}
-          height={200}
-          alt="company logos"
-          className="w-32 sm:w-40 md:w-48 lg:w-52"
+          className="w-64"
         />
       </article>
     </Container>
